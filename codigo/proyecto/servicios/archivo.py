f34a890d2b634001b9d90fb53177b55c6c8520c6
VanderleyCastro <vanderleyosiriscastrocuadrado@gmail.com>
2026-04-01 12:25:24 -0500

    Update archivo.py

--- codigo/proyecto/servicios/archivo.py
+++ codigo/proyecto/servicios/archivo.py
@@ -2,15 +2,15 @@ import json
 import os
 
 estudiantes = []
-data = "datos/estudiantes.json"
+save = "datos/estudiantes.json"
 
 def load_student():
-    if not os.path.exists(data):
+    if not os.path.exists(save):
         return []
-    with open(data, "r", encoding="utf-8") as file:
+    with open(save, "r", encoding="utf-8") as file:
         return json.load(file)
 
 def save_student(estudiantes):
     os.makedirs("datos",exist_ok=True)
-    with open(data, "w", encoding="utf-8") as file:
+    with open(save, "w", encoding="utf-8") as file:
         json.dump(estudiantes, file, ensure_ascii=False, indent=4)
